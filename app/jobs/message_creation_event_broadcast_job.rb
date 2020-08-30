21af2c3810251d3feebb8f4009684a51786699e9
BartTheScrivner <dframe22@gmail.com>
2020-08-30 18:24:52 -0400

    got subs linked to match chats

--- app/jobs/message_creation_event_broadcast_job.rb
+++ app/jobs/message_creation_event_broadcast_job.rb
@@ -2,12 +2,14 @@ class MessageCreationEventBroadcastJob < ApplicationJob
     queue_as :default
   
     def perform(message)
-      ActionCable
-        .server
-        .broadcast('match_chat_channel',
-          user_id: message.user_id,
-          match_chat_id: message.match_chat_id,
-          created_at: message.created_at.strftime('%I:%M:%S %p'),
-          content: message.content)
+      match_chat = MatchChat.find(message.match_chat_id)
+      MatchChatChannel.broadcast_to(match_chat, message)
+      # ActionCable
+      #   .server
+      #   .broadcast('match_chat_channel',
+      #     user_id: message.user_id,
+      #     match_chat_id: message.match_chat_id,
+      #     created_at: message.created_at.strftime('%I:%M:%S %p'),
+      #     content: message.content)
     end
   end
