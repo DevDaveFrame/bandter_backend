b87b64a9787fe886bb175cd58fcd1cc5ae2d7322
BartTheScrivner <dframe22@gmail.com>
2020-08-31 11:52:49 -0400

    tweaked serializers

--- app/serializers/match_chat_serializer.rb
+++ app/serializers/match_chat_serializer.rb
@@ -1,7 +1,7 @@
 class MatchChatSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :friender_id, :friendee_id, :accepted, :friender_name, :friendee_name, :friender_img, :friendee_img
-  has_many :messages
+  has_many :messages, serializer: MessageSerializer
   belongs_to :friender, record_type: :user
   belongs_to :friendee, record_type: :user
 
