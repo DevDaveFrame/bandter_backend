95e99bb0c6fe22c56c40dbccd59e3cb145d6b15d
BartTheScrivner <dframe22@gmail.com>
2020-08-31 22:02:08 -0400

    fixed syntax in matchchats controller

--- app/controllers/api/v1/match_chats_controller.rb
+++ app/controllers/api/v1/match_chats_controller.rb
@@ -1,11 +1,12 @@
-class MatchChatsController < ApplicationController
+class Api::V1::MatchChatsController < ApplicationController
   def index
     match_chats = MatchChat.all 
-    render json: {match_chats}
+    render json: match_chats
   end 
 
   def show
     match_chat = MatchChat.find(params[:id])
-    render json: {MatchChatSerializer.new(match_chat)}
+    options = {include: [:messages]}
+    render json: MatchChatSerializer.new(match_chat, options)
   end
 end
