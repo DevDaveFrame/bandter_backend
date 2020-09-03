0c6d36dcd7b5b911359a91b8cc2557fa9c60c599
BartTheScrivner <dframe22@gmail.com>
2020-09-03 17:25:58 -0400

    cleaned up broadcast job

--- app/jobs/message_creation_event_broadcast_job.rb
+++ app/jobs/message_creation_event_broadcast_job.rb
@@ -4,12 +4,5 @@ class MessageCreationEventBroadcastJob < ApplicationJob
     def perform(message)
       match_chat = MatchChat.find(message.match_chat_id)
       MatchChatChannel.broadcast_to(match_chat, MessageSerializer.new(message))
-      # ActionCable
-      #   .server
-      #   .broadcast('match_chat_channel',
-      #     user_id: message.user_id,
-      #     match_chat_id: message.match_chat_id,
-      #     created_at: message.created_at.strftime('%I:%M:%S %p'),
-      #     content: message.content)
     end
   end
