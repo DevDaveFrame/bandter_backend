bd7d9ecc9d4f30770ccf32ef3133a220c0f3b853
BartTheScrivner <dframe22@gmail.com>
2020-09-06 15:16:44 -0400

    added url to songs

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::SongsController < ApplicationController
   skip_before_action :logged_in?, only: [:create, :update]
   def create
-    song = Song.new(user_id: params[:user_id], genre_id: 860)
-    song.save
+    song = Song.create(user_id: params[:user_id], genre_id: 860)
     song.audio.attach(params[:audio])
+    url = url_for(song.audio)
+    song.update(content: url)
     render json: {message: "SUPER DEE DUPER!"}
   end
 
