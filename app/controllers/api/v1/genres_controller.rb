5cd0d0f33ef403e00c8bfd2025313019b083b487
BartTheScrivner <dframe22@gmail.com>
2020-09-06 18:16:27 -0400

    genre serialized

--- app/controllers/api/v1/genres_controller.rb
+++ app/controllers/api/v1/genres_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::GenresController < ApplicationController
+  skip_before_action :logged_in?, only: [:index, :update]
   def index
     genres = Genre.all
-    render json: genres
+    render json: genres, only: [:id, :name]
   end
 end
