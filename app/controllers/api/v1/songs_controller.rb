27af92a30afabb1e4f0a62aa46f1f93ff93b1099
BartTheScrivner <dframe22@gmail.com>
2020-09-06 14:27:43 -0400

    started songs controller

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,2 +1,13 @@
-class SongsController < ApplicationController
+class Api::V1::SongsController < ApplicationController
+  skip_before_action :logged_in?, only: [:create, :update]
+  def create
+    song = Song.new(user_id: params[:user_id], genre_id: 860)
+    song.save
+    song.audio.attach(params[:audio])
+    render json: {message: "SUPER DEE DUPER!"}
+  end
+
+  def show
+    song = Song.find(params[:id])
+    render 
 end
