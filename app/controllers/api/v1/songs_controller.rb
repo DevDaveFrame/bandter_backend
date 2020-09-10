01f2f329016eabc790f867d43ad1b52d1a3fd013
BartTheScrivner <dframe22@gmail.com>
2020-09-09 22:08:19 -0400

    added additional params for songs

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::SongsController < ApplicationController
   skip_before_action :logged_in?, only: [:create, :update]
   def create
-    song = Song.create(user_id: params[:user_id], genre_id: 860)
+    song = Song.create(user_id: params[:user_id], title:params[:title], album:params[:album], genre_id: 860)
     song.audio.attach(params[:audio])
     url = url_for(song.audio)
     song.update(description: url)
-    render json: {message: "SUPER DEE DUPER!"}
+    render json: SongSerializer.new(song).serializable_hash
   end
 
   def show
